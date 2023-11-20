87f780f49d96401ef5ab04d6113d8f7db2676551
HaysR <57479934+RomanSavh@users.noreply.github.com>
2023-11-20 22:24:15 +0200

    pl formulas update

--- trading-sdk-mt-engine/src/flows/active_positions/update_active_rate.rs
+++ trading-sdk-mt-engine/src/flows/active_positions/update_active_rate.rs
@@ -1,4 +1,4 @@
-use crate::{get_close_price, get_open_price, MtBidAsk, MtPosition, MtPositionActiveState};
+use crate::{get_close_price, MtBidAsk, MtPosition, MtPositionActiveState};
 
 pub fn update_active_position_rate(
     position: &mut MtPosition<MtPositionActiveState>,
@@ -14,20 +14,95 @@ pub fn update_active_position_rate(
         return;
     }
 
-    if position.base_data.quote == new_bid_ask.base
-        && position.base_data.collateral == new_bid_ask.quote
+    if (position.base_data.quote == new_bid_ask.base
+        || position.base_data.quote == new_bid_ask.quote)
+        && (position.base_data.base == new_bid_ask.base
+            || position.base_data.base == new_bid_ask.quote)
     {
         position.state.quote_collateral_active_price =
             get_close_price(new_bid_ask, &position.base_data.side);
         position.state.quote_collateral_active_bid_ask = Some(new_bid_ask.clone());
     }
+}
 
-    if position.base_data.quote == new_bid_ask.quote
-        && position.base_data.collateral == new_bid_ask.base
-    {
-        position.state.quote_collateral_active_price =
-            get_close_price(new_bid_ask, &position.base_data.side)
-                / get_open_price(new_bid_ask, &position.base_data.side);
-        position.state.quote_collateral_active_bid_ask = Some(new_bid_ask.clone());
+mod test {
+    use rust_extensions::date_time::DateTimeAsMicroseconds;
+
+    use crate::{
+        get_open_price, update_active_position_rate, update_position_pl, MtBidAsk, MtPosition,
+        MtPositionActiveState, MtPositionActiveStateOpenData, MtPositionBaseData, MtPositionSwaps,
+    };
+
+    #[test]
+    fn rate_update_bug_467() {
+        let asset_bid_ask: MtBidAsk = MtBidAsk {
+            asset_pair: "USDMXN".to_string(),
+            bid: 5555.0,
+            ask: 5555.0,
+            base: "USD".to_string(),
+            quote: "MXN".to_string(),
+            date: DateTimeAsMicroseconds::now(),
+        };
+
+        let base_data = MtPositionBaseData {
+            id: "id".to_string(),
+            trader_id: "trader_id".to_string(),
+            account_id: "account_id".to_string(),
+            asset_pair: "USDMXN".to_string(),
+            side: crate::MtPositionSide::Buy,
+            invest_amount: 50.0,
+            leverage: 5.0,
+            stop_out_percent: 30.0,
+            create_process_id: "process".to_string(),
+            crate_date: DateTimeAsMicroseconds::now(),
+            last_update_process_id: "process".to_string(),
+            last_update_date: DateTimeAsMicroseconds::now(),
+            collateral: "USD".to_string(),
+            base: "USD".to_string(),
+            quote: "MXN".to_string(),
+            tp_profit: None,
+            tp_price: None,
+            sl_profit: None,
+            sl_price: None,
+        };
+
+        let open_data: MtPositionActiveStateOpenData = MtPositionActiveStateOpenData {
+            asset_open_price: get_open_price(&asset_bid_ask, &crate::MtPositionSide::Buy),
+            asset_open_bid_ask: asset_bid_ask.clone(),
+            base_collateral_open_price: 1.0,
+            base_collateral_open_bid_ask: None,
+            open_process_id: "process".to_string(),
+            open_date: DateTimeAsMicroseconds::now(),
+            pending_state: None,
+        };
+
+        let active_state = MtPositionActiveState {
+            open_data,
+            asset_active_price: 0.0,
+            asset_active_bid_ask: asset_bid_ask.clone(),
+            quote_collateral_active_price: 0.0,
+            quote_collateral_active_bid_ask: None,
+            profit: 0.0,
+            swaps: MtPositionSwaps::default(),
+        };
+
+        let mut position = MtPosition {
+            state: active_state,
+            base_data,
+        };
+
+        let close_asset_bid_ask = MtBidAsk {
+            asset_pair: "USDMXN".to_string(),
+            bid: 17.919,
+            ask: 17.9267,
+            base: "USD".to_string(),
+            quote: "MXN".to_string(),
+            date: DateTimeAsMicroseconds::now(),
+        };
+
+        update_active_position_rate(&mut position, &close_asset_bid_ask);
+        println!("{:#?}", position);
+
+        // assert_eq!(format!("{:.2}", position.state.profit), 18.71.to_string());
     }
 }
--- trading-sdk-mt-engine/src/flows/calculations/calculate_pl.rs
+++ trading-sdk-mt-engine/src/flows/calculations/calculate_pl.rs
@@ -1,39 +1,88 @@
 use crate::{MtPosition, MtPositionActiveState, MtPositionSide};
 
-pub fn update_position_pl(position: &mut MtPosition<MtPositionActiveState>) {
+pub fn update_unrealized_pl(position: &mut MtPosition<MtPositionActiveState>) {
+    let is_inverted_instrument = position.base_data.quote != position.base_data.collateral;
+
+    let side_coefficient = match position.base_data.side {
+        MtPositionSide::Buy => 1.0,
+        MtPositionSide::Sell => -1.0,
+    };
+
     let volume = position.base_data.invest_amount * position.base_data.leverage;
 
-    let collateral_amount = match &position.state.open_data.base_collateral_open_bid_ask {
-        Some(collateral_open_bid_ask) => {
-            if position.base_data.collateral != collateral_open_bid_ask.quote {
-                volume * position.state.open_data.base_collateral_open_price
-            } else {
-                volume / position.state.open_data.base_collateral_open_price
-            }
-        }
-        None => volume,
+    let investment_volume = match is_inverted_instrument {
+        true => volume,
+        false => volume / position.state.open_data.asset_open_price,
     };
 
     let price_change =
         position.state.asset_active_price - position.state.open_data.asset_open_price;
 
-    let pl = match &position.state.quote_collateral_active_bid_ask {
-        Some(quote_collateral_bid_ask) => {
-            if position.base_data.quote != quote_collateral_bid_ask.quote {
-                collateral_amount * price_change * position.state.quote_collateral_active_price
-            } else {
-                collateral_amount * price_change / position.state.quote_collateral_active_price
-            }
-        }
-        None => collateral_amount * price_change,
+    let instrument_profit = investment_volume * price_change;
+
+    let collateral_profit = match is_inverted_instrument {
+        true => instrument_profit / position.state.asset_active_price,
+        false => instrument_profit,
     };
 
-    let pl = match position.base_data.side {
-        MtPositionSide::Buy => pl,
-        MtPositionSide::Sell => -pl,
+    position.state.profit = collateral_profit * side_coefficient;
+}
+
+pub fn update_collateral_unrealized_pl(position: &mut MtPosition<MtPositionActiveState>) {
+    let inverted_base = position
+        .state
+        .open_data
+        .base_collateral_open_bid_ask
+        .as_ref()
+        .unwrap()
+        .quote
+        != position.base_data.collateral;
+    let volume = position.base_data.invest_amount * position.base_data.leverage;
+
+    let investment_volume = match inverted_base {
+        true => volume * position.state.open_data.base_collateral_open_price,
+        false => volume / position.state.open_data.base_collateral_open_price,
     };
 
-    position.state.profit = pl;
+    let price_change =
+        position.state.asset_active_price - position.state.open_data.asset_open_price;
+
+    let base_profit = investment_volume * price_change;
+
+    //account currency profit
+
+    let inverted_quote = position
+        .state
+        .quote_collateral_active_bid_ask
+        .as_ref()
+        .unwrap()
+        .quote
+        != position.base_data.collateral;
+
+    let collateral_currency_profit = match inverted_quote {
+        true => base_profit / position.state.quote_collateral_active_price,
+        false => base_profit * position.state.quote_collateral_active_price,
+    };
+
+    let side_coefficient = match position.base_data.side {
+        MtPositionSide::Buy => 1.0,
+        MtPositionSide::Sell => -1.0,
+    };
+
+
+    position.state.profit = collateral_currency_profit * side_coefficient;
+
+
+}
+
+pub fn update_position_pl(position: &mut MtPosition<MtPositionActiveState>) {
+    if position.base_data.base == position.base_data.collateral
+        || position.base_data.quote == position.base_data.collateral
+    {
+        update_unrealized_pl(position);
+    } else {
+        update_collateral_unrealized_pl(position)
+    }
 }
 
 #[cfg(test)]
@@ -41,8 +90,8 @@ mod tests {
     use rust_extensions::date_time::DateTimeAsMicroseconds;
 
     use crate::{
-        get_close_price, get_open_price, update_position_pl, MtBidAsk, MtPositionActiveState,
-        MtPositionActiveStateOpenData, MtPositionBaseData, MtPositionSwaps, MtPosition,
+        get_close_price, get_open_price, update_position_pl, MtBidAsk, MtPosition,
+        MtPositionActiveState, MtPositionActiveStateOpenData, MtPositionBaseData, MtPositionSwaps,
     };
 
     #[test]
@@ -107,7 +156,7 @@ mod tests {
             swaps: MtPositionSwaps::default(),
         };
 
-        let mut position = MtPosition{
+        let mut position = MtPosition {
             state: active_state,
             base_data,
         };
@@ -115,5 +164,170 @@ mod tests {
         update_position_pl(&mut position);
 
         assert_eq!(format!("{:.2}", position.state.profit), 18.71.to_string());
+    }
+
+    #[test]
+    fn calculate_pl_2() {
+        let asset_bid_ask = MtBidAsk {
+            asset_pair: "USDCAD".to_string(),
+            bid: 1.32162,
+            ask: 1.32166,
+            base: "USD".to_string(),
+            quote: "CAD".to_string(),
+            date: DateTimeAsMicroseconds::now(),
+        };
+
+        let base_data = MtPositionBaseData {
+            id: "id".to_string(),
+            trader_id: "trader_id".to_string(),
+            account_id: "account_id".to_string(),
+            asset_pair: "USDCAD".to_string(),
+            side: crate::MtPositionSide::Buy,
+            invest_amount: 1000.0,
+            leverage: 20.0,
+            stop_out_percent: 30.0,
+            create_process_id: "process".to_string(),
+            crate_date: DateTimeAsMicroseconds::now(),
+            last_update_process_id: "process".to_string(),
+            last_update_date: DateTimeAsMicroseconds::now(),
+            collateral: "USD".to_string(),
+            base: "USD".to_string(),
+            quote: "CAD".to_string(),
+            tp_profit: None,
+            tp_price: None,
+            sl_profit: None,
+            sl_price: None,
+        };
+
+        let open_data: MtPositionActiveStateOpenData = MtPositionActiveStateOpenData {
+            asset_open_price: get_open_price(&asset_bid_ask, &crate::MtPositionSide::Buy),
+            asset_open_bid_ask: asset_bid_ask.clone(),
+            base_collateral_open_price: 1.0,
+            base_collateral_open_bid_ask: None,
+            open_process_id: "process".to_string(),
+            open_date: DateTimeAsMicroseconds::now(),
+            pending_state: None,
+        };
+
+        let close_asset_bid_ask = MtBidAsk {
+            asset_pair: "USDCAD".to_string(),
+            bid: 1.34398,
+            ask: 1.34402,
+            base: "USD".to_string(),
+            quote: "CAD".to_string(),
+            date: DateTimeAsMicroseconds::now(),
+        };
+
+        let active_state = MtPositionActiveState {
+            open_data,
+            asset_active_price: get_close_price(&close_asset_bid_ask, &base_data.side),
+            asset_active_bid_ask: close_asset_bid_ask,
+            quote_collateral_active_price: 1.0,
+            quote_collateral_active_bid_ask: None,
+            profit: 0.0,
+            swaps: MtPositionSwaps::default(),
+        };
+
+        let mut position = MtPosition {
+            state: active_state,
+            base_data,
+        };
+
+        update_position_pl(&mut position);
+
+        assert_eq!(format!("{:.4}", position.state.profit), 332.1478.to_string());
+    }
+
+
+    #[test]
+    fn calculate_pl_3() {
+        let asset_bid_ask = MtBidAsk {
+            asset_pair: "GBPCAD".to_string(),
+            bid: 1.64432,
+            ask: 1.64447,
+            base: "GBP".to_string(),
+            quote: "CAD".to_string(),
+            date: DateTimeAsMicroseconds::now(),
+        };
+
+        let base_collateral_bid_ask = MtBidAsk {
+            asset_pair: "GBPUSD".to_string(),
+            bid: 1.248,
+            ask: 1.2482,
+            base: "GBP".to_string(),
+            quote: "USD".to_string(),
+            date: DateTimeAsMicroseconds::now(),
+        };
+
+
+
+        let base_data = MtPositionBaseData {
+            id: "id".to_string(),
+            trader_id: "trader_id".to_string(),
+            account_id: "account_id".to_string(),
+            asset_pair: "GBPCAD".to_string(),
+            side: crate::MtPositionSide::Buy,
+            invest_amount: 1000.0,
+            leverage: 20.0,
+            stop_out_percent: 30.0,
+            create_process_id: "process".to_string(),
+            crate_date: DateTimeAsMicroseconds::now(),
+            last_update_process_id: "process".to_string(),
+            last_update_date: DateTimeAsMicroseconds::now(),
+            collateral: "USD".to_string(),
+            base: "GBP".to_string(),
+            quote: "CAD".to_string(),
+            tp_profit: None,
+            tp_price: None,
+            sl_profit: None,
+            sl_price: None,
+        };
+
+        let open_data: MtPositionActiveStateOpenData = MtPositionActiveStateOpenData {
+            asset_open_price: get_open_price(&asset_bid_ask, &crate::MtPositionSide::Buy),
+            asset_open_bid_ask: asset_bid_ask.clone(),
+            base_collateral_open_price: get_open_price(&base_collateral_bid_ask, &crate::MtPositionSide::Buy),
+            base_collateral_open_bid_ask: Some(base_collateral_bid_ask.clone()),
+            open_process_id: "process".to_string(),
+            open_date: DateTimeAsMicroseconds::now(),
+            pending_state: None,
+        };
+
+        let close_asset_bid_ask = MtBidAsk {
+            asset_pair: "GBPCAD".to_string(),
+            bid: 1.62432,
+            ask: 1.62447,
+            base: "GBP".to_string(),
+            quote: "CAD".to_string(),
+            date: DateTimeAsMicroseconds::now(),
+        };
+
+        let close_quote_collateral_bid_ask = MtBidAsk {
+            asset_pair: "USDCAD".to_string(),
+            bid: 1.34398,
+            ask: 1.34402,
+            base: "USD".to_string(),
+            quote: "CAD".to_string(),
+            date: DateTimeAsMicroseconds::now(),
+        };
+
+        let active_state = MtPositionActiveState {
+            open_data,
+            asset_active_price: get_close_price(&close_asset_bid_ask, &base_data.side),
+            asset_active_bid_ask: close_asset_bid_ask,
+            quote_collateral_active_price: get_close_price(&close_quote_collateral_bid_ask, &crate::MtPositionSide::Buy),
+            quote_collateral_active_bid_ask: Some(close_quote_collateral_bid_ask.clone()),
+            profit: 0.0,
+            swaps: MtPositionSwaps::default(),
+        };
+
+        let mut position = MtPosition {
+            state: active_state,
+            base_data,
+        };
+
+        update_position_pl(&mut position);
+
+        assert_eq!(format!("{:.4}", position.state.profit), (-240.2305).to_string());
     }
 }
